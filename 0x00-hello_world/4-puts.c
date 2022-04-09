407160fd97db6489ab803950d02f0b877375ab0d
TFrankie55 <towoeye50@gmail.com>
2022-04-09 14:18:01 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,10 @@
 /**
  * main - Entry point
  *
- * Description: puts "Programming is like building a multilingual puzzle"
- *
  * Return: Always 0 (Success)
  * */
-int main(void)
+int main(viod)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		return (0);
 }
