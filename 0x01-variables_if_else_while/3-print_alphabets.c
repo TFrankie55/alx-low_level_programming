03a8c864ea623bfc0b6026c3dd1f27f3ff85717c
TFrankie55 <towoeye50@gmail.com>
2022-04-11 07:56:48 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Deacription: prints the letters in lower then uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	i = 97;
+	j = 65;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+
+	while (j < 91)
+	{
+		putchar(j);
+		j++;
+	}
+
+	putchar(10);
+
+	return (10);
+}
