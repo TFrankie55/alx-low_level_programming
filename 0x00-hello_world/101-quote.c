f523c1180299ea2cd4ae8b4d1491ec5e897abd13
TFrankie55 <towoeye50@gmail.com>
2022-04-11 12:14:21 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,14 @@
 #include <unistd.h>
 
 /**
- * main - prints Dora's quote
+ * main - Prints Dora's quote
  *
  * Description: prints "and that piece of art is useful" - Dora Korpar
  *
- * Return = Always 1 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
