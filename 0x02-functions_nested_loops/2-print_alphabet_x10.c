07c567a6533b4f208edfc0acf42af5d39b96b0e0
TFrankie55 <towoeye50@gmail.com>
2022-04-12 14:52:04 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints alphabet lower case
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+int alphabet;
+int count;
+
+
+count = 0
+while (count < 10)
+{
+for (alphabet = 'a' ; alphabeet <= 'z' ; alphabet++)
+{
+	_putchar(alphabet);
+}
+
+count++;
+_putchar('\n');
+}
+
+}
