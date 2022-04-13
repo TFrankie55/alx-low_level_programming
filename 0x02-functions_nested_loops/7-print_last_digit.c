4290dc1eeda00003f4be5d9e9c8ec35bb62d95c2
TFrankie55 <towoeye50@gmail.com>
2022-04-12 17:43:43 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,16 +12,16 @@ int last_digit;
 
 if (n < 0)
 {
-last digit = (-1 * (n % 10));
+last_digit = (-1 * (n % 10));
 _putchar (last_digit + '0');
 return (last_digit);
 }
 
 else
 {
-last digit = (n % 10);
+last_digit = (n % 10);
 _putchar (last_digit + '0');
-return (last digit);
+return (last_digit);
 }
 
 }
