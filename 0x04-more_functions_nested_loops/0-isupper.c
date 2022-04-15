5ce83463fce32721d5980fd2692e108689dd07c4
TFrankie55 <towoeye50@gmail.com>
2022-04-14 21:54:20 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase character
+ * @letter: the character to be tracked
+ * Return: 1 if c is uppercase, otherwise 0
+ */
+
+int _isupper(int letter)
+{
+	return (letter >= 'A' && letter <= 'Z');
+}
