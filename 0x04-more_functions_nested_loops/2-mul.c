2ea1c2d29981bd34e4307727adfd4fcdae6359ff
TFrankie55 <towoeye50@gmail.com>
2022-04-15 15:02:27 -0700

    Task 3

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,11 +4,11 @@
  * main - function that multiplies two integers
  * @a: int to be multiplied to b
  * @b: int to be multiplied to a
- * Return: Always 0 (Success)
+ * Return: the result of the operation
  */
 int mul(int a, int b)
 {
-	int c;
-	c = a * b;
-	return (c);
+int c;
+c = a * b;
+return (c);
 }
