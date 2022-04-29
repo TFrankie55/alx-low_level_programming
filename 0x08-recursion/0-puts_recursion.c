0f9d49a7db8f5804f998e130259b253b0da1ab2e
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2022-04-29 19:41:04 +0100

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,7 +5,7 @@
  * @s: string
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately
+ * On error, -1 is returned, and errno is set appropriately.
  */
 
 void _puts_recursion(char *s)
