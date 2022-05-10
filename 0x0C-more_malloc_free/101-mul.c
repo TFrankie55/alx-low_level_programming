4758260d40029cfa9017d9fecbf1e9a9600c3006
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2022-05-10 14:18:41 +0100

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,7 +11,7 @@
 int _atoi_digit(char x)
 {
 	unsigned int res;
-
+	
 	if (x <= '9' && x >= '0')
 		res = x - '0';
 	return (res);
@@ -25,7 +25,6 @@ int _atoi_digit(char x)
 int _isNumber(char *argv)
 {
 	int i;
-
 	for (i = 0; argv[i]; i++)
 		if (argv[i] < 48 || argv[i] > 57)
 			return (1);
@@ -43,15 +42,15 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *tab;
 	unsigned int i;
-
+	
 	tab = malloc(size * nmemb);
-
+	
 	if (tab == NULL)
 		return (NULL);
-
+	
 	for (i = 0; i < (size * nmemb); i++)
 		tab[i] = '0';
-
+	
 	return (tab);
 }
 
@@ -68,7 +67,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 void *mul_array(char *a1, int len1, char a2, char *a3, int lena)
 {
 	int mul = 0, i, k;
-
+	
 	k = lena;
 	for (i = len1 - 1; i >= 0 ; i--)
 	{
@@ -77,15 +76,15 @@ void *mul_array(char *a1, int len1, char a2, char *a3, int lena)
 		mul /= 10;
 		k--;
 	}
-
-		while (mul != 0)
-		{
-			mul += a3[k] - '0';
-			a3[k] = (mul % 10) + '0';
-			mul /= 10;
-			k--;
-		}
-
+	
+	while (mul != 0)
+	{
+		mul += a3[k] - '0';
+		a3[k] = (mul % 10) + '0';
+		mul /= 10;
+		k--;
+	}
+	
 	return (a3);
 }
 /**
@@ -96,7 +95,7 @@ void *mul_array(char *a1, int len1, char a2, char *a3, int lena)
 void print_array(char *a, int nb)
 {
 	int i = 0;
-
+	
 	while (a[i] == '0' && (i + 1) < nb)
 	{
 		i++;
@@ -120,7 +119,7 @@ int main(int argc, char *argv[])
 	int i, c, len1, len2, lenres;
 	char E[6] = {'E', 'r', 'r', 'o', 'r', '\n'};
 	char *tabres;
-
+	
 	if (argc != 3 || _isNumber(argv[1]) == 1 || _isNumber(argv[2]) == 1)
 	{
 		for (i = 0; i < 6; i++)
@@ -130,9 +129,9 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	for (len1 = 0; argv[1][len1]; len1++)
-	;
+		;
 	for (len2 = 0; argv[2][len2]; len2++)
-	;
+		;
 	lenres = len1 + len2;
 	tabres = _calloc(lenres, sizeof(int));
 	if (tabres == NULL)
@@ -142,8 +141,8 @@ int main(int argc, char *argv[])
 	}
 	for (i = len2 - 1, c = 0; i >= 0; i--)
 	{
-	tabres = mul_array(argv[1], len1, argv[2][i], tabres, (lenres - 1 - c));
-	c++;
+		tabres = mul_array(argv[1], len1, argv[2][i], tabres, (lenres - 1 - c));
+		c++;
 	}
 	print_array(tabres, lenres);
 	free(tabres);
