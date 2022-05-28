3a2afdfa237a81b15e4147e6298753b38e3083dc
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2022-05-28 02:01:52 +0100

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,28 +1,78 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_listint_safe - the main function
- * @head: linked list in front
+ * add_nodeaddress - Add a node to a list with
+ * @head: Pointer to the pointer of the start of the list
+ * @address: The address of another list's node
  *
- * Return: i
+ * Return: Address of new node, NULL if it fails
+ */
+listadd_t *add_nodeaddress(listadd_t **head, const listint_t *address)
+{
+	listadd_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		free_listadd(*head);
+		exit(98);
+	}
+	new->address = (void *)address;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+
+/**
+ * free_listadd - Free a list
+ * @head: Pointer to the start of the list
+ */
+void free_listadd(listadd_t *head)
+{
+	listadd_t *killnode;
+
+	while (head != NULL)
+	{
+		killnode = head;
+		head = head->next;
+		free(killnode);
+	}
+}
+
+/**
+ * print_listint_safe - Print out a given list, but only once if it loops
+ * @head: Pointer to the start of the list
+ *
+ * Return: Number of nodes, if it fails print 98
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t i = 0;
-	const listint_t *temp, *node;
+	listadd_t *newhead;
+	listadd_t *checker;
+	unsigned int count;
 
-	node = head;
-	while (node != NULL)
+	count = 0;
+	newhead = NULL;
+	while (head != NULL)
 	{
-		printf("[%p] %d\n", (void *)node, node->n);
-		temp = node;
-		node = node->next;
-		i++;
-		if (temp <= node)
+		checker = newhead;
+		while (checker != NULL)
 		{
-			printf("-> [%p] %d\n", (void *)node, node->n);
-			break;
+			if (head == checker->address)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listadd(newhead);
+				return (count);
+			}
+			checker = checker->next;
 		}
+		printf("[%p] %d\n", (void *)head, head->n);
+		add_nodeaddress(&newhead, head);
+		head = head->next;
+		count++;
 	}
-	return (i);
+	free_listadd(newhead);
+	return (count);
 }
