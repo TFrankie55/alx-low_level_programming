78fab8a2bf18ad9b009e516422b8f0e701b8d1bd
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2022-06-01 05:19:27 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,40 +1,31 @@
 #include "main.h"
 
-
 /**
- * binary_to_uint - function to convert binary to uint
- * @b: binary string to be converted
- * @length: string array length
- * @sum: variable to hold conversion result
- * @power: variable to raise and lower power
- * @j: count variable
- * Return: 0, if b contains unidentified char or b is NULL
- *		the converted number if b is valid
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: A pointer to a string of 0 and 1 chars.
+ *
+ * Return: If b is NULL or contains chars not 0 or 1 - 0.
+ *         Otherwise - the converted number.
  */
- 
- unsigned int binary_to_uint(const char *b)
- {
-	unsigned int sum = 0, power = 1;
-	int length, j;
-	
-	if(b)
-	{
-		for(length = 0; b[length];)
-		{
-			length++;
-		}
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0, mult = 1;
+	int len;
 
-		for (j = length-1; j >= 0; j--)
-		{	
-			sum += (b[j] - '0') * power;
-			power *= 2;
-		}
-
-		return (sum);
-	}
-	else 
-	{
+	if (b == '\0')
 		return (0);
+
+	for (len = 0; b[len];)
+		len++;
+
+	for (len -= 1; len >= 0; len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+
+		num += (b[len] - '0') * mult;
+		mult *= 2;
 	}
 
- }
+	return (num);
+}
