3c15b614b4866a0eafb1a8b732a3d94b151d3a8b
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2022-06-02 14:18:13 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,10 +2,10 @@
 
 /**
  * read_textfile - reads a text file and prints the letters
- * @filename: filename
- * @letters: numbers of letters printed
+ * @filename: filename.
+ * @letters: numbers of letters printed.
  *
- * Return: number of letters printed. If it fails, returns 0
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buf;
 
 	if (!filename)
-	return (0);
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 
@@ -26,7 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, rd);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
 	close(fd);
 
