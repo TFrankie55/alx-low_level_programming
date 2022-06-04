62c443d3d4745baa0e3fc5a6ec707791e4e04fe5
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2022-06-04 09:08:17 +0100

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-/**
- * b - prints from init by the loader before main
- */
+void first(void) __attribute__ ((constructor));
 
-void b(void) __attribute__ ((constructor));
-void b(void)
+/**
+ * first - prints a sentence before the main
+ * function is executed
+ */
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
